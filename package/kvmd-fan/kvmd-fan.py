9287dacb7ff86c97cd8db4a520945b5ee24a20f8
PARK <parkkw472@gmail.com>
2022-12-04 17:09:44 +0900

    Update kvmd-fan.py
    Code refactoring, removing unused variables, improving code.
    
    By adding the idle_speed variable, the idle fan operation speed function added.
    
    Added min_temp and max_temp variables to control PWM by calculating the percentage between min and max temperature.
    
    Also for safety, if the temperature rises above 60 degrees, the fan speed will be 100%.

--- package/kvmd-fan/kvmd-fan.py
+++ package/kvmd-fan/kvmd-fan.py
@@ -3,47 +3,43 @@
 
 import RPi.GPIO
 import time
+
+#settings
+fan_gpio=12 # Fan GPIO pin number (default: 12)
+idle_speed = 0 # Fan speed when under min_temp(IDLE) (min: 0, max: 100)
+min_temp = 40 # Fan starting temperature 
+max_temp = 60 # Fan max speed temperature (max: 60)
+
+#define GPIO
 RPi.GPIO.setwarnings(False)
 RPi.GPIO.setmode(RPi.GPIO.BCM)
-RPi.GPIO.setup(12, RPi.GPIO.OUT)
-pwm = RPi.GPIO.PWM(12,100)
-RPi.GPIO.setwarnings(False)
+RPi.GPIO.setup(fan_gpio, RPi.GPIO.OUT)
+pwm = RPi.GPIO.PWM(fan_gpio,100)
+running = False
 
-
-speed = 0
-prv_temp = 0
-target_temp = 55
-target_max_speed = 80
-fan_p = 10
-fan_flag = False
-
+#running code
 try:
+	while True:
+		tmpFile = open( '/sys/class/thermal/thermal_zone0/temp' )
+		cpu_temp = int(tmpFile.read())/1000
+		tmpFile.close()
+		print("Temp:",cpu_temp)
+		if (cpu_temp >= 60) or (cpu_temp > min_temp): 
+			if(running): #if fan not running, start fan
+				pwm.start(0)
+				running = True
+			if (cpu_temp >= 60): #For safety, temp exceeds 60 degrees, the fan speed is always set to 100%.
+				set_speed = 100
+			else:
+				temp_speed = int((cpu_temp - min_temp) / (max_temp - min_temp) * 100) # set 0~100% for min and max temp ranges
+				set_speed = min(max(idle_speed, temp_speed), 100) # make speed not over 100 
+			print("set_speed:",set_speed,"%") 
+			pwm.ChangeDutyCycle(set_speed)
+		else :
+			if(running):
+				pwm.stop()
 
-
-		while True:
-		
-				tmpFile = open( '/sys/class/thermal/thermal_zone0/temp' )
-				cpu_temp = int(tmpFile.read())/1000
-				tmpFile.close()
-				print("temp:%d",cpu_temp)
-				if (cpu_temp>=60) or (prv_temp > target_temp and fan_flag == True) :
-					fan_flag = True
-					if prv_temp<60 :
-						pwm.start(0)
-						pwm.ChangeDutyCycle(100)
-						time.sleep(.1)
-					temp_gap = cpu_temp - target_temp
-					speed = fan_p * temp_gap
-					#set_speed = min( speed , target_max_speed )
-					set_speed = 85
-					print("set_speed:%d",set_speed)
-					pwm.ChangeDutyCycle(set_speed)
-				else :
-					fan_flag = False
-					pwm.stop()
-				prv_temp = cpu_temp
-				
-				time.sleep(5)
+		time.sleep(5)
 				
 except KeyboardInterrupt:
 		pass
