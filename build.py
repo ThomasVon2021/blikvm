4a507adf9d0268c171ac301b8cf24fa257d595cb
shangbinbin <shangbinbin@192.168.10.9>
2023-01-14 19:39:22 +0800

    add package py script

---
+++ build.py
@@ -0,0 +1,41 @@
+# eg: python3 build.py --platform x86
+import argparse
+import os
+import subprocess
+import json
+
+gArgs = None
+def doArgParse():
+    global gArgs
+    optParser = argparse.ArgumentParser(description='build and tar file')
+    optParser.add_argument('--platform', nargs='?', default='', type=str, help='x86 or arm')
+    gArgs = optParser.parse_args()
+
+def main():
+    doArgParse()
+    sh_path = os.path.split(os.path.realpath(__file__))[0]
+    make_path = sh_path + '/src'
+
+    # build demo.bin
+    cmd = "make PLATFORM=" + gArgs.platform
+    output = subprocess.check_output(cmd, shell = True, cwd=make_path )
+
+    # package binary  rm -rf release && rm -r release.tar.gz && 
+    cmd = "mkdir release && cp package/kvmd-hid/* release/ && cp package/kvmd-main/* release/ && \
+    cp package/kvmd-oled/* release/ && cp package/kvmd-web/* release/ && cp package/ustreamer/* release/ && \
+    tar -zcvf release.tar.gz release && rm -rf release"
+    output = subprocess.check_output(cmd, shell = True, cwd=sh_path )
+
+    # modify package.json
+    file_path = sh_path + "/src/config/package.json"
+    with open(file_path,'r',encoding='utf8')as fp_r:
+        json_data = json.load(fp_r)
+        cmd = "md5sum release.tar.gz"
+        output = subprocess.check_output(cmd, shell = True, cwd=sh_path )
+        print("md5: ",output, "str: ", output.decode('utf-8')[0:32] )
+        json_data['md5value'] = output.decode('utf-8')[0:32]
+        with open(file_path,'w',encoding='utf8')as fp_w:
+	        json.dump(json_data,fp_w,ensure_ascii=False)
+
+if __name__ == '__main__':
+    main()
