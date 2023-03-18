41efcfadaa823b38b8fca199a071bc30f5056717
刀客 <wangyueliang_007@163.com>
2023-03-18 15:43:25 +0800

    Update install-kvmd-msd.sh

--- package/kvmd-msd/install-kvmd-msd.sh
+++ package/kvmd-msd/install-kvmd-msd.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+
+if [ ! -d  "opt/bin/msd/user" ] 
+then
+  sudo mddir -p /opt/bin/msd/user/
+fi
+
+if [ ! -d  "opt/bin/msd/ventoy" ] 
+then
+  sudo mddir -p /opt/bin/msd/ventoy/
+fi
+
+if [ ! -d  "opt/bin/msd/ventoy" ] 
+then
 mkdir -p "/usr/bin/blikvm"
+fi
+
 cp kvmd-msd.sh "/usr/bin/blikvm/kvmd-msd.sh"
 cp -r ventoy-1.0.88 "/usr/bin/blikvm/"
 
