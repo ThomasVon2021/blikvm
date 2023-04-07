9cf5f8036f5eb1a7e209054349917d8046da812b
Thomas <45224459+ThomasVon2021@users.noreply.github.com>
2023-04-07 23:40:58 +0800

    Update install-kvmd-msd.sh

--- package/kvmd-msd/install-kvmd-msd.sh
+++ package/kvmd-msd/install-kvmd-msd.sh
@@ -3,17 +3,17 @@
 
 if [ ! -d  "opt/bin/msd/user" ] 
 then
-  sudo mkdir -p /opt/bin/msd/user/
+  mkdir -p /opt/bin/msd/user/
 fi
 
 if [ ! -d  "opt/bin/msd/ventoy" ] 
 then
-  sudo mkdir -p /opt/bin/msd/ventoy/
+  mkdir -p /opt/bin/msd/ventoy/
 fi
 
 if [ ! -d  "/usr/bin/blikvm" ] 
 then
-  sudo mkdir -p /usr/bin/blikvm
+  mkdir -p /usr/bin/blikvm
 fi
 
 cp kvmd-msd.sh "/usr/bin/blikvm/kvmd-msd.sh"
@@ -21,4 +21,5 @@ cp -r ventoy-1.0.88 "/usr/bin/blikvm/"
 
 chmod 755 -R /opt/bin/msd/
 chmod 755 /usr/bin/blikvm/kvmd-msd.sh
+echo "install msd ok"
 
