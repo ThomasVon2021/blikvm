49f77737c4e3b88e2c81dde37a0132d1333212ec
swordsman <wangyueliang_007@163.com>
2023-04-16 11:22:06 +0800

    msd: support cycle param

--- package/kvmd-msd/README.md
+++ package/kvmd-msd/README.md
@@ -21,11 +21,11 @@
 4. excute msd cmd.wait until excute end. The default size of the USB flash disk is 5GB, if your iso is large than 5GB, you should modify kvmd-msd.sh.
 - If /opt/bin/msd/user this path only have one iso, you can use the follow command. 
 ```
-	sudo bash /usr/bin/blikvm/kvmd-msd.sh
+	sudo bash /usr/bin/blikvm/kvmd-msd.sh make
 ```
 - If /opt/bin/msd/user this path have more than one iso, you can use the follow command. **xxx.iso** means the iso name.
 ```
-	sudo bash /usr/bin/blikvm/kvmd-msd.sh xxx.iso
+	sudo bash /usr/bin/blikvm/kvmd-msd.sh make xxx.iso
 ```
 
 **Tips: cp progress slowly, Please be patient.**
@@ -33,16 +33,21 @@
 5. If you want to expand the USB flash size, find "sudo dd if=/dev/zero of=ventoy.img bs=1M count=5120 status=progress;" this line in kvmd-msd.sh, change the "count=5120". 
 
 	
-6. excute disable_gadget.sh
+6. connect msd
 ```
-	sudo bash /usr/bin/blikvm/disable-gadget.sh
-```
-		
-7. reconnect img, excute enable_gadget.sh
-```
-	sudo bash /usr/bin/blikvm/enable-gadget.sh
+	sudo bash  /usr/bin/blikvm/kvmd-msd.sh conn
 ```
 		
-8. throuht web restart PC, enter BIOS, modify boot priority,set blikvm USB first.
+7. disconnect msd
+```
+	sudo bash  /usr/bin/blikvm/kvmd-msd.sh disconn
+```
 
-9. according to step, select operate system , format partition and install system.
+8. clean msd
+```
+	sudo bash  /usr/bin/blikvm/kvmd-msd.sh clean
+```
+		
+9. throuht web restart PC, enter BIOS, modify boot priority,set blikvm USB first.
+
+10. according to step, select operate system , format partition and install system.
--- package/kvmd-msd/kvmd-msd.sh
+++ package/kvmd-msd/kvmd-msd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CMD=$1
+FILE=$2
+
 set -e
 
 set -x
@@ -10,92 +13,96 @@ iso_file_name=()
 ventoy_dir="/opt/bin/msd/ventoy"
 iso_dir="/opt/bin/msd/user"
 mount_dist_dir="/media/blikvm/ventoy/"
+usb_gadget_sh="/usr/bin/blikvm/enable-gadget.sh"
+usb_dis_gadget_sh="/usr/bin/blikvm/disable-gadget.sh"
 
-
-if [ $# -gt 1 ]
-then
-	echo "param need point one, or default 0 use iso all img."
-fi
+CMD=${CMD:-"unset"}
 
 traverse_dir()
 {
-    filepath=$1
-    
-    for file in `ls -a $filepath`
-    do
-        if [ -d ${filepath}/$file ]
-        then
-            if [[ $file != '.' && $file != '..' ]]
-            then
-                traverse_dir ${filepath}/$file
-            fi
-        else
-            check_suffix ${filepath}/$file
-        fi
-    done
+	filepath=$1
+	
+	for file in `ls -a $filepath`
+	do
+		if [ -d ${filepath}/$file ]
+		then
+			if [[ $file != '.' && $file != '..' ]]
+			then
+				traverse_dir ${filepath}/$file
+			fi
+		else
+			check_suffix ${filepath}/$file
+		fi
+	done
 }
- 
- 
+
+
 ##get iso file
 check_suffix()
 {
-    file=$1
-    
-    if [ "${file##*.}"x = "iso"x ] ;then
-        #echo $(basename $file)
-		echo "storage: $file"
-		iso_file_name[${#iso_file_name[*]}]=$file
-    fi    
+	file=$1
+	
+	# if [ "${file##*.}"x = "iso"x ] ;then
+	#echo $(basename $file)
+	echo "storage: $file"
+	iso_file_name[${#iso_file_name[*]}]=$file
+	# fi    
 }
 
-echo "blikvm" | sudo -S mount -o remount,rw /
 
-if [ -f  "$ventoy_dir/ventoy.img" ] 
-then
-	echo "update file exist"
-	cd  $ventoy_dir
-else
-	if [ ! -d  $ventoy_dir ]
+case ${CMD} in
+	make)
+	if [ $# -gt 2 ]
 	then
-		echo "blikvm" | sudo -S mkdir -p  $ventoy_dir
+		echo "param need point one, or default 0 use iso all img."
 	fi
-	cd  $ventoy_dir
-	echo "update file not exist,please wait..."
-    sudo dd if=/dev/zero of=ventoy.img bs=1M count=5120 status=progress;
-fi
 
-echo "blikvm" | sudo -S losetup -f ventoy.img
+	echo "blikvm" | sudo -S mount -o remount,rw /
 
-echo "blikvm" | sudo -S losetup -l | grep ventoy
-
-dev_name=`echo "blikvm" | sudo -S losetup -l | grep ventoy | awk 'NR==1{print $1}'`
-
-echo $dev_name
-
-echo "blikvm" | sudo -S sh /usr/bin/blikvm/ventoy-1.0.88/Ventoy2Disk.sh -i $dev_name;
-
-echo "blikvm" | sudo -S mkdir -p $mount_dist_dir
-
-echo "blikvm" | sudo -S mount  $dev_name"p1" $mount_dist_dir;
-
-if [ $? -ne 0 ]
-then
-	echo "mount  $dev_name"p1" fail"
-fi
-
-#sudo cp -r "/usr/bin/blikvm/ventoy-1.0.88/plugin/ventoy" "$mount_dist_dir"
-
-#if [ $? -eq 0 ]
-#then
-#	echo "cp plugin ventoy sucess!!!"
-#fi
-
-echo $PWD
-
-if [ $# == 1 ];then
-	file_name=$1
-    if [ "${file_name##*.}"x = "iso"x ]
+	if [ -f  "$ventoy_dir/ventoy.img" ] 
 	then
+		echo "update file exist"
+		cd  $ventoy_dir
+	else
+		if [ ! -d  $ventoy_dir ]
+		then
+			echo "blikvm" | sudo -S mkdir -p  $ventoy_dir
+		fi
+		cd  $ventoy_dir
+		echo "update file not exist,please wait..."
+		sudo dd if=/dev/zero of=ventoy.img bs=1M count=5120 status=progress;
+	fi
+
+	echo "blikvm" | sudo -S losetup -f ventoy.img
+
+	echo "blikvm" | sudo -S losetup -l | grep ventoy
+
+	dev_name=`echo "blikvm" | sudo -S losetup -l | grep ventoy | awk 'NR==1{print $1}'`
+
+	echo $dev_name
+
+	echo "blikvm" | sudo -S sh /usr/bin/blikvm/ventoy-1.0.88/Ventoy2Disk.sh -i $dev_name;
+
+	echo "blikvm" | sudo -S mkdir -p $mount_dist_dir
+
+	echo "blikvm" | sudo -S mount  $dev_name"p1" $mount_dist_dir;
+
+	if [ $? -ne 0 ]
+	then
+		echo "mount  $dev_name"p1" fail"
+	fi
+
+	#sudo cp -r "/usr/bin/blikvm/ventoy-1.0.88/plugin/ventoy" "$mount_dist_dir"
+
+	#if [ $? -eq 0 ]
+	#then
+	#	echo "cp plugin ventoy sucess!!!"
+	#fi
+
+	echo $PWD
+
+	if [ $# == 2 ];then
+		file_name=${FILE}
 		echo "param correct"
 		echo $mount_dist_dir${file_name##*/}
 		if [[ -f $mount_dist_dir${file_name##*/} && -f "$iso_dir/${file_name}" ]]
@@ -115,37 +122,67 @@ if [ $# == 1 ];then
 			echo "cp $iso_dir/${file_name} sucess!"
 		fi
 		sync
-		
+	elif  [ $# == 1 ];then
+		traverse_dir $iso_dir
+		for name in ${iso_file_name[@]}
+		do
+			#echo ${name##*/}
+			echo $mount_dist_dir${name##*/}
+			if [ -f $mount_dist_dir${name##*/} ]
+			then
+				echo "exist update file: ${name},rm it "
+				echo "blikvm" | sudo -S rm -f $mount_dist_dir${name##*/}
+				sync
+			fi
+			sleep 3
+			echo "${name} again!"
+			cp "${name}" "$mount_dist_dir";
+			if [ $? -ne 0 ]
+			then
+				echo "default cp failed"
+			else 
+				echo "default cp ${name} sucess!"
+			fi
+			sync
+		done
 	else
-		echo "param suffix is not iso"
+		echo "sorry, param error, need one, current: $#"
 		return 1
 	fi
-elif  [ $# == 0 ];then
-	traverse_dir $iso_dir
-	for name in ${iso_file_name[@]}
-	do
-		#echo ${name##*/}
-		echo $mount_dist_dir${name##*/}
-		if [ -f $mount_dist_dir${name##*/} ]
-		then
-			echo "exist update file: ${name},rm it "
-			echo "blikvm" | sudo -S rm -f $mount_dist_dir${name##*/}
-			sync
-		fi
-		sleep 3
-		echo "${name} again!"
-		cp "${name}" "$mount_dist_dir";
-		if [ $? -ne 0 ]
-		then
-			echo "default cp failed"
-		else 
-			echo "default cp ${name} sucess!"
-		fi
-		sync
-	done
-else
-	echo "sorry, param error, need one, current: $#"
-	return 1
-fi
-umount -f $dev_name"p1"
-losetup -d $dev_name
+	umount -f $dev_name"p1"
+	losetup -d $dev_name
+
+	;;
+
+	conn)
+	cd  $ventoy_dir
+	if [ -f  "$ventoy_dir/ventoy.img_bak" ] 
+	then
+		echo "blikvm" | sudo -S mv ventoy.img_bak ventoy.img
+	fi
+	bash $usb_dis_gadget_sh
+	bash $usb_gadget_sh
+	;;
+
+	disconn)
+	cd  $ventoy_dir
+	if [ -f  "$ventoy_dir/ventoy.img" ] 
+	then
+		echo "blikvm" | sudo -S mv ventoy.img ventoy.img_bak
+	fi
+	bash $usb_dis_gadget_sh
+	bash $usb_gadget_sh
+	;;
+
+	clean)
+	if [[ -f  "$ventoy_dir/ventoy.img" || -f  "$ventoy_dir/ventoy.img_bak" ]]
+	then
+		echo "blikvm" | sudo -S rm -f $ventoy_dir/ventoy.*
+	fi
+	bash $usb_dis_gadget_sh
+	bash $usb_gadget_sh
+	;;
+	*)
+	echo "unset param, please use param: make,conn,disconn..."
+	;;
+esac
