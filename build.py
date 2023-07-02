384128d9ec289ad8f472f7508c4d200cd294a083
ThomasVon2021 <1767893964@qq.com>
2023-07-02 05:47:48 -0230

    opt v4 kvm-link

--- build.py
+++ build.py
@@ -26,12 +26,15 @@ def main():
     # build demo.bin
     cmd_make = ""
     cmd_pack = "mkdir release && "
+    file_tar_name = ""
     if gArgs.platform == "pi":
         cmd_make += "make RPI=1 SSD1306=1"
         cmd_pack += "cp package/kvmd-web/binary/pi/* release/ && cp package/ustreamer/binary/pi/* release/ && "
+        file_tar_name = "release.tar.gz"
     elif gArgs.platform == "h616":
         cmd_make += "make H616=1 ST7789=1"
         cmd_pack += "cp package/kvmd-web/binary/h616/* release/ && cp package/ustreamer/binary/h616/* release/ && "
+        file_tar_name = "release-h616-v4.tar.gz"
     else:
         print("input error platform")
         return
@@ -44,7 +47,8 @@ def main():
         find package/ustreamer/ -maxdepth 1 -type f -exec cp {} release/ \; && \
         cp src/kvmd-main release/ && cp -R package/kvmd-msd/* release/ && \
         cp script/install_release.py release/ && \
-        cp src/config/package.json release/ && tar -zcvf release.tar.gz release && rm -rf release"
+        cp src/config/package.json release/ && "
+    cmd_pack += "tar -zcvf " + file_tar_name + " release && rm -rf release"
     
     print("pack relase package, command: ", cmd_pack)
     output = subprocess.check_output(cmd_pack, shell = True, cwd=sh_path )
@@ -54,7 +58,7 @@ def main():
     file_path = sh_path + "/src/config/package.json"
     with open(file_path,'r',encoding='utf8')as fp_r:
         json_data = json.load(fp_r)
-        cmd = "md5sum release.tar.gz"
+        cmd = "md5sum " + file_tar_name
         output = subprocess.check_output(cmd, shell = True, cwd=sh_path )
         # print("md5: ",output, "str: ", output.decode('utf-8')[0:32] )
         json_data['md5value'] = output.decode('utf-8')[0:32]
