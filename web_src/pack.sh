fb013f0aca293e4cf2912457f45f05544a599cfc
ThomasVon2021 <1767893964@qq.com>
2024-08-26 15:29:06 +0800

    rename web-client to web_client, also rename web-server to web_server

--- web_src/pack.sh
+++ web_src/pack.sh
@@ -4,10 +4,10 @@
 cd web-client
 npm install
 npm run build
-cp -r dist ../web-server
+cp -r dist ../web_server
 cd ../
 
 # build server
-cd ./web-server
+cd ./web_server
 npm run clean
 npm run build
